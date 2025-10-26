080cd5416f53331a0609d08303fc5ca36ce2c6f5
Gianluca Tallè <gianlucatalle96@gmail.com>
2025-10-26 09:09:09 +0100

    fix: modern accessor syntax

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -6,17 +6,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Link extends Model
 {
     use HasFactory, SoftDeletes;
     protected $appends = ['short_url'];
 
-    // questo accessor mi aiuta a ottenere ogni volta l'url completo
-    public function getShortUrlAttribute(): string 
+    // questo accessor mi aiuta a ottenere ogni volta l'url completo dal db
+    protected function ShortUrl (): Attribute
     {
-        return url($this->short_code);
-    }
+        return Attribute::make(
+            get: fn () => url($this->short_code)
+        );
+    } 
 
     public function clicks(): HasMany
     {
